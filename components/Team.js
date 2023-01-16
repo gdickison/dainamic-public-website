47a3cf6df669bd97516558e5f0f526c751471f37
paunchymonk <gregory@dickison.net>
2023-01-16 12:36:31 -0800

    Change team to leadership and only include three individuals

--- components/Team.js
+++ components/Team.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import teamData from "../public/data/team-data.json"
+import leadershipData from "../public/data/leadership-data.json"
 import TeamMemberCard from "./TeamMemberCard"
 
 export default function Team () {
@@ -8,11 +8,11 @@ export default function Team () {
     <section className="text-gray-600 body-font relative">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
-          <h1 className="text-2xl font-medium uppercase mb-4 text-gray-900">Meet our team</h1>
+          <h1 className="text-2xl font-medium uppercase mb-4 text-gray-900">Leadership</h1>
         </div>
-        <div className="flex flex-wrap -m-4">
-          {teamData &&
-            teamData.map(teamMember => (
+        <div className="flex flex-wrap -m-4 justify-center">
+          {leadershipData &&
+            leadershipData.map(teamMember => (
               <TeamMemberCard
                 key={teamMember.id}
                 name={teamMember.name}
