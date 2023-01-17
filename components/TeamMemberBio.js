40d92d817f1b1840f11fd04fa0c8bee80cc8b543
paunchymonk <gregory@dickison.net>
2023-01-17 08:41:04 -0800

    Center bio modal vertically and add margin to top of image

--- components/TeamMemberBio.js
+++ components/TeamMemberBio.js
@@ -2,11 +2,11 @@
 export default function TeamMemberBio ({name, role, image, text, social, handleClick}) {
   return (
     <section className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full h-full bg-slate-500 bg-opacity-50 z-10">
-      <div className="relative p-[30px] max-w-5xl my-[100px] mx-auto bg-white rounded-md">
+      <div className="relative md:top-1/2 md:-translate-y-1/2 p-[30px] max-w-5xl mx-auto bg-white rounded-md">
       <img className="absolute h-6 w-6 top-2 right-2 md:h-10 md:w-10 md:top-6 md:right-6 hover:cursor-pointer" src="/images/x-close.svg" alt="close" onClick={handleClick} />
-        <div className="flex flex-col md:flex-row w-full overflow-scroll">
+        <div className="flex flex-col md:flex-row w-full gap-4 md:gap-0 overflow-scroll">
           <div className="w-full">
-            <img className="w-full mx-auto sm:max-w-xs" src={`/images/${image}`} alt="" />
+            <img className="w-full mt-2 mx-auto sm:max-w-xs" src={`/images/${image}`} alt="" />
           </div>
           <div className="md:max-w-[60%]">
             <div className="flex items-center gap-6">
