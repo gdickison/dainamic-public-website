5f955ed0b823fb6e03bc34374d1cdd98daed9838
paunchymonk <gregory@dickison.net>
2023-01-17 08:32:18 -0800

    Postition modal better, remove red border, add background

--- components/TeamMemberBio.js
+++ components/TeamMemberBio.js
@@ -1,32 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 export default function TeamMemberBio ({name, role, image, text, social, handleClick}) {
   return (
-    <section className=" bg-white border-2 border-red-500 fixed inset-0 md:top-1/2 md:left-1/2 md:-translate-y-1/2 md:-translate-x-1/2 md:overflow-y-auto h-fit z-10">
-      <div className="relative transform overflow-hidden rounded-lg shadow-xl transition-all">
-        <img className="absolute h-6 w-6 top-2 right-2 md:h-10 md:w-10 md:top-6 md:right-6 hover:cursor-pointer" src="/images/x-close.svg" alt="close" onClick={handleClick} />
-        <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 my-8 md:my-20 gap-y-6 md:grid-cols-2 gap-x-10 content-center">
-            <div className="">
-              <img className="w-full mx-auto sm:max-w-xs" src={`/images/${image}`} alt="" />
-            </div>
-            <div className="">
-              <div className="flex items-center gap-6">
-                <h3 className="text-xl font-semibold text-gray-900">
-                  {name}
-                </h3>
-                <span>
-                  <a className="text-gray-500" href={social} target="_blank" rel="noreferrer">
-                    <img className="h-8 w-8" src="/images/linkedin.png" alt="linked in icon" />
-                  </a>
-                </span>
-              </div>
-              <h3 className="text-lg text-gray-900">
-                {role}
+    <section className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full h-full bg-slate-500 bg-opacity-50 z-10">
+      <div className="relative p-[30px] max-w-5xl my-[100px] mx-auto bg-white rounded-md">
+      <img className="absolute h-6 w-6 top-2 right-2 md:h-10 md:w-10 md:top-6 md:right-6 hover:cursor-pointer" src="/images/x-close.svg" alt="close" onClick={handleClick} />
+        <div className="flex flex-col md:flex-row w-full overflow-scroll">
+          <div className="w-full">
+            <img className="w-full mx-auto sm:max-w-xs" src={`/images/${image}`} alt="" />
+          </div>
+          <div className="md:max-w-[60%]">
+            <div className="flex items-center gap-6">
+              <h3 className="text-xl font-semibold text-gray-900">
+                {name}
               </h3>
-              <p className="mt-4 text-lg text-gray-700">
-                {text}
-              </p>
+              <span>
+                <a className="text-gray-500" href={social} target="_blank" rel="noreferrer">
+                  <img className="h-8 w-8" src="/images/linkedin.png" alt="linked in icon" />
+                </a>
+              </span>
             </div>
+            <h3 className="text-lg text-gray-900">
+              {role}
+            </h3>
+            <p className="mt-4 text-base text-gray-700">
+              {text}
+            </p>
           </div>
         </div>
       </div>
