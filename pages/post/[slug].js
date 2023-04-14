3ffa8bdd53089c879b6093601f8173367036537a
paunchymonk <gregory@dickison.net>
2023-04-14 16:57:08 -0700

    Add some styling to the blog posts

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
@@ -11,31 +12,37 @@ export default function BlogStandard () {
   return (
     <>
       {post &&
-        <article className="px-4 py-24 mx-auto max-w-7xl" itemid="#" itemscope itemtype="http://schema.org/BlogPosting">
+        <article className="px-4 py-24 mx-auto max-w-7xl" itemid="#" itemScope itemType="http://schema.org/BlogPosting">
+          <section className="text-gray-600 body-font bg-[url('/images/bg_abstract_white.jpg')] bg-cover">
+            <div className="container mx-auto flex px-5 pt-12 pb-5 md:flex-row flex-col items-center">
+              <div className="w-full lg:flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <p className="mb-3 text-xs font-semibold tracking-wider text-gray-500 uppercase">
+                  {post.categories}
+                </p>
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{post.title}</h1>
+                <p className="text-gray-700 w-full">
+                  <span className="byline author vcard" itemProp="author" itemScope="itemScope" itemType="http://schema.org/Person">
+                    <Link href="/about" itemProp="url" className="text-primary hover:text-primary-dark">
+                    <span itemProp="name">
+                      {post.author}
+                    </span>
+                    </Link>
+                  </span>
+                </p>
+                <p className="text-gray-700 italic">
+                  <span className="byline author vcard" itemProp="author" itemScope="itemScope" itemType="http://schema.org/Person">
+                    <span itemProp="name">
+                      {post.role}
+                    </span>
+                  </span>
+                </p>
+              </div>
+              <div className="w-full">
+                <img className="object-cover object-center rounded" alt={post.author} src={`/images/${post.authorImage}`}/>
+              </div>
+            </div>
+          </section>
           <div className="w-full mx-auto mb-12 text-center md:w-2/3">
-            <p className="mb-3 text-xs font-semibold tracking-wider text-gray-500 uppercase">
-              {post.categories}
-            </p>
-            <h1 className="mb-3 text-3xl font-bold text-gray-900 md:leading-tight md:text-4xl" itemprop="headline" title="Rise of Tailwind - A Utility First CSS Framework">
-              {post.title}
-            </h1>
-            <p className="text-gray-700">
-              <span className="byline author vcard" itemprop="author" itemscope="itemscope" itemtype="http://schema.org/Person">
-                {/* <a href="mailto:hello@dainamic.ai" target="_blank" itemprop="url" rel="author noopener noreferrer" className="text-primary hover:text-primary-dark"> */}
-                <Link href="/about" itemprop="url" className="text-primary hover:text-primary-dark">
-                <span itemprop="name">
-                  {post.author}
-                </span>
-                </Link>
-              </span>
-            </p>
-            <p className="text-gray-700 italic">
-              <span className="byline author vcard" itemprop="author" itemscope="itemscope" itemtype="http://schema.org/Person">
-                <span itemprop="name">
-                  {post.role}
-                </span>
-              </span>
-            </p>
           </div>
           <div className="mx-auto prose text-lg">
             <div dangerouslySetInnerHTML={{__html:post.text}} className="space-y-3"></div>
