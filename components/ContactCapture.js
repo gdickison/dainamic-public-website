4cecf310cf92d30b845ae86639ac2763d55756ad
paunchymonk <gregory@dickison.net>
2023-08-18 13:09:19 -0700

    Make name and email required for whitepaper download

--- components/ContactCapture.js
+++ components/ContactCapture.js
@@ -94,6 +94,7 @@ export default function ContactCapture () {
                       type="text"
                       name="first-name"
                       id="first-name"
+                      required
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -109,6 +110,7 @@ export default function ContactCapture () {
                       type="text"
                       name="last-name"
                       id="last-name"
+                      required
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -124,6 +126,7 @@ export default function ContactCapture () {
                       id="email"
                       name="email"
                       type="email"
+                      required
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       placeholder="We promise not to spam you or give your information to anyone"
                     />
